ee8067da29de149b3026c4577aeb47b0dc61b01e
Dmitry <crismsfiva@gmail.com>
2020-02-03 18:36:00 +0300

    implement and import lib for comparing JSON files

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
 import program from 'commander';
+import genDiff from '../index.js';
 
 program
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((pathToFile1, pathToFile2) => {
+    console.log(genDiff(pathToFile1, pathToFile2));
+  });
 
 program.parse(process.argv);
