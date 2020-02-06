16b41fbe1c805b86f0da0466ba3f459c853d7e51
Dmitry <crismsfiva@gmail.com>
2020-02-06 20:25:39 +0300

    add test for comparing yaml files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,10 +5,16 @@ import gendiff from '../src';
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const path1 = getFixturePath('before.json');
-const path2 = getFixturePath('after.json');
 const result = readFile('result.txt').trim();
 
 test('Comparing two JSON files', () => {
+  const path1 = getFixturePath('before.json');
+  const path2 = getFixturePath('after.json');
+  expect(gendiff(path1, path2)).toBe(result);
+});
+
+test('Comparing two yaml files', () => {
+  const path1 = getFixturePath('before.yml');
+  const path2 = getFixturePath('after.yml');
   expect(gendiff(path1, path2)).toBe(result);
 });
