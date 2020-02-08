041eed0343b03b62542615338b65879477f48a21
Dmitry <crismsfiva@gmail.com>
2020-02-08 11:55:18 +0300

    add ini parser

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,12 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 export default (extension) => {
   const map = {
     json: (path) => JSON.parse(fs.readFileSync(path, 'utf-8')),
     yml: (path) => yaml.safeLoad(fs.readFileSync(path, 'utf-8')),
+    ini: (path) => ini.parse(fs.readFileSync(path, 'utf-8')),
   };
 
   return map[extension];
