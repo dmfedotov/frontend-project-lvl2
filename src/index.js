d5c0d8e55fb2f0c1c08229cb144f7d739f624c81
Dmitry <crismsfiva@gmail.com>
2020-02-09 14:48:42 +0300

    make the right paths to files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import path from 'path';
-import _ from 'lodash';
+import { difference, has } from 'lodash';
 import parser from './parsers';
 
 
 export default (path1, path2) => {
   const extension = path.extname(path1).slice(1);
-  const file1 = parser(extension)(path1);
-  const file2 = parser(extension)(path2);
+  const pathToFile1 = path.resolve(process.cwd(), path1);
+  const pathToFile2 = path.resolve(process.cwd(), path2);
 
-  const secondFileNewKeys = _.difference(Object.keys(file2), Object.keys(file1));
+  const file1 = parser(extension)(pathToFile1);
+  const file2 = parser(extension)(pathToFile2);
+
+  const secondFileNewKeys = difference(Object.keys(file2), Object.keys(file1));
 
   const diffArray = Object.entries(file1).reduce((acc, [key, value]) => {
-    if (_.has(file2, key)) {
+    if (has(file2, key)) {
       if (value === file2[key]) {
         return [...acc, [`    ${key}: ${value}`]];
       }
