b2e7871cfac4979c0ead83516b83419ef5580d31
Dmitry <crismsfiva@gmail.com>
2020-02-11 22:25:42 +0300

    refactor entry point file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import path from 'path';
-import { difference, has } from 'lodash';
+import buildDiff from './diff';
 import parser from './parsers';
-
+import render from './render';
 
 export default (path1, path2) => {
   const extension = path.extname(path1).slice(1);
@@ -10,27 +10,7 @@ export default (path1, path2) => {
 
   const file1 = parser(extension)(pathToFile1);
   const file2 = parser(extension)(pathToFile2);
+  const diffTree = buildDiff(file1, file2);
 
-  const secondFileNewKeys = difference(Object.keys(file2), Object.keys(file1));
-
-  const diffArray = Object.entries(file1).reduce((acc, [key, value]) => {
-    if (has(file2, key)) {
-      if (value === file2[key]) {
-        return [...acc, [`    ${key}: ${value}`]];
-      }
-      return [
-        ...acc,
-        [`  + ${key}: ${file2[key]}`],
-        [`  - ${key}: ${value}`],
-      ];
-    }
-
-    return [...acc, [`  - ${key}: ${value}`]];
-  }, []);
-
-  const newValues = secondFileNewKeys.map((key) => `  + ${key}: ${file2[key]}`);
-  const resultDiffArray = diffArray.concat(newValues);
-  const resultDiffString = resultDiffArray.join('\n');
-
-  return `{\n${resultDiffString}\n}`;
+  return render(diffTree);
 };
