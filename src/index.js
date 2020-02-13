89a48a08280d178fa3fbb7ef312aaddd1fa314ba
Dmitry <crismsfiva@gmail.com>
2020-02-13 18:17:47 +0300

    refactor entry point according to formats

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import path from 'path';
-import buildDiff from './diff';
+import buildAst from './diff';
 import parser from './parsers';
-import render from './render';
+import render from './formatters';
 
-export default (path1, path2) => {
+export default (path1, path2, format = 'complex') => {
   const extension = path.extname(path1).slice(1);
   const pathToFile1 = path.resolve(process.cwd(), path1);
   const pathToFile2 = path.resolve(process.cwd(), path2);
 
   const file1 = parser(extension)(pathToFile1);
   const file2 = parser(extension)(pathToFile2);
-  const diffTree = buildDiff(file1, file2);
+  const diffAst = buildAst(file1, file2);
 
-  return render(diffTree);
+  return render[format](diffAst);
 };
