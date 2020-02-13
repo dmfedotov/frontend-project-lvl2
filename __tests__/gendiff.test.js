1fb5ea77809f5229435f448e47ad50a78b36a733
Dmitry <crismsfiva@gmail.com>
2020-02-13 18:14:10 +0300

    modify test depending formats

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,12 +5,26 @@ import gendiff from '../src';
 const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const result = readFile('result').trim();
+describe('Comparing files in complex format', () => {
+  const complex = readFile('complex').trim();
 
-test.each([
-  ['before.json', 'after.json', result],
-  ['before.yml', 'after.yml', result],
-  ['before.ini', 'after.ini', result],
-])('Comparing %s and %s files', (path1, path2, expected) => {
-  expect(gendiff(getFixturePath(path1), getFixturePath(path2))).toBe(expected);
+  test.each([
+    ['before.json', 'after.json', complex],
+    ['before.yml', 'after.yml', complex],
+    ['before.ini', 'after.ini', complex],
+  ])('%s and %s', (path1, path2, expected) => {
+    expect(gendiff(getFixturePath(path1), getFixturePath(path2))).toBe(expected);
+  });
+});
+
+describe('Comparing files in plain format', () => {
+  const plain = readFile('plain').trim();
+
+  test.each([
+    ['before.json', 'after.json', 'plain', plain],
+    ['before.yml', 'after.yml', 'plain', plain],
+    ['before.ini', 'after.ini', 'plain', plain],
+  ])('%s and %s', (path1, path2, format, expected) => {
+    expect(gendiff(getFixturePath(path1), getFixturePath(path2), format)).toBe(expected);
+  });
 });
