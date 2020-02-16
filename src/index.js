ca7aaee0bb22611ae3c4aaac9b72857ef4ad030c
Dmitry <crismsfiva@gmail.com>
2020-02-16 18:02:01 +0300

    refactor parser signature

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ export default (path1, path2, format = 'complex') => {
   const pathToFile1 = path.resolve(process.cwd(), path1);
   const pathToFile2 = path.resolve(process.cwd(), path2);
 
-  const file1 = parser(extension)(pathToFile1);
-  const file2 = parser(extension)(pathToFile2);
+  const file1 = parser(extension, pathToFile1);
+  const file2 = parser(extension, pathToFile2);
   const diffAst = buildAst(file1, file2);
 
   return render[format](diffAst);
--- src/parsers.js
+++ src/parsers.js
@@ -2,12 +2,12 @@ import fs from 'fs';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-export default (extension) => {
+export default (extension, pathToFile) => {
   const map = {
     json: (path) => JSON.parse(fs.readFileSync(path, 'utf-8')),
     yml: (path) => yaml.safeLoad(fs.readFileSync(path, 'utf-8')),
     ini: (path) => ini.parse(fs.readFileSync(path, 'utf-8')),
   };
 
-  return map[extension];
+  return map[extension](pathToFile);
 };
