761362433a6c9c7bb95ddfb7a7706fb46bc7c9a3
Dmitry <crismsfiva@gmail.com>
2020-02-16 17:03:31 +0300

    remove duplication in tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,39 +5,22 @@ import gendiff from '../src';
 const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-describe('Comparing files in complex format', () => {
+describe('Comparing files in different formats', () => {
   const complex = readFile('complex').trim();
-
-  test.each([
-    ['before.json', 'after.json', complex],
-    ['before.yml', 'after.yml', complex],
-    ['before.ini', 'after.ini', complex],
-  ])('%s and %s', (path1, path2, expected) => {
-    expect(gendiff(getFixturePath(path1), getFixturePath(path2))).toBe(expected);
-  });
-});
-
-
-describe('Comparing files in plain format', () => {
   const plain = readFile('plain').trim();
-
-  test.each([
-    ['before.json', 'after.json', 'plain', plain],
-    ['before.yml', 'after.yml', 'plain', plain],
-    ['before.ini', 'after.ini', 'plain', plain],
-  ])('%s and %s', (path1, path2, format, expected) => {
-    expect(gendiff(getFixturePath(path1), getFixturePath(path2), format)).toBe(expected);
-  });
-});
-
-describe('Comparing files in JSON format', () => {
   const json = readFile('json').trim();
 
   test.each([
+    ['before.json', 'after.json', 'complex', complex],
+    ['before.yml', 'after.yml', 'complex', complex],
+    ['before.ini', 'after.ini', 'complex', complex],
+    ['before.json', 'after.json', 'plain', plain],
+    ['before.yml', 'after.yml', 'plain', plain],
+    ['before.ini', 'after.ini', 'plain', plain],
     ['before.json', 'after.json', 'json', json],
     ['before.yml', 'after.yml', 'json', json],
     ['before.ini', 'after.ini', 'json', json],
-  ])('%s and %s', (path1, path2, format, expected) => {
-    expect(gendiff(getFixturePath(path1), getFixturePath(path2), format)).toBe(expected);
+  ])('%s and %s in %s format', (file1, file2, format, expected) => {
+    expect(gendiff(getFixturePath(file1), getFixturePath(file2), format)).toBe(expected);
   });
 });
