45e192f45900ca804230b484502cc37e263583cf
Dmitry <crismsfiva@gmail.com>
2020-02-17 20:48:55 +0300

    refactor ast building

--- src/diff.js
+++ src/diff.js
@@ -1,36 +1,40 @@
-import { union, isEqual } from 'lodash';
+import { union, isEqual, isObject } from 'lodash';
 
-const iter = (data1, data2, acc) => {
-  const uniqKeys = union(Object.keys(data1), Object.keys(data2)).sort();
+const buildNode = (name, ...values) => {
+  const [valueBefore, valueAfter] = values;
 
-  return uniqKeys.reduce((innerAcc, key) => {
+  if (!valueBefore) {
+    return { name, type: 'added', valueAfter };
+  }
+  if (!valueAfter) {
+    return { name, type: 'deleted', valueBefore };
+  }
+
+  return {
+    name, type: 'changed', valueBefore, valueAfter,
+  };
+};
+
+const buildAst = (data1, data2, acc) => {
+  const keys = union(Object.keys(data1), Object.keys(data2)).sort();
+
+  const ast = keys.map((key) => {
     const valueBefore = data1[key];
     const valueAfter = data2[key];
-    const hasChildren = valueBefore instanceof Object && valueAfter instanceof Object;
+    const hasChildren = isObject(valueBefore) && isObject(valueAfter);
 
     if (!isEqual(valueBefore, valueAfter)) {
       if (hasChildren) {
-        return [...innerAcc,
-          {
-            name: key,
-            type: 'unchanged',
-            children: iter(valueBefore, valueAfter, []),
-          },
-        ];
+        return { name: key, type: 'nested', children: buildAst(valueBefore, valueAfter, []) };
       }
-      if (typeof valueBefore === 'undefined' && typeof valueAfter !== 'undefined') {
-        return [...innerAcc, { name: key, type: 'added', valueAfter }];
-      }
-      if (typeof valueBefore !== 'undefined' && typeof valueAfter === 'undefined') {
-        return [...innerAcc, { name: key, type: 'deleted', valueBefore }];
-      }
-      return [...innerAcc, {
-        name: key, type: 'changed', valueBefore, valueAfter,
-      }];
+
+      return buildNode(key, valueBefore, valueAfter);
     }
 
-    return [...innerAcc, { name: key, type: 'unchanged', value: valueBefore }];
+    return { name: key, type: 'unchanged', value: valueBefore };
   }, acc);
+
+  return ast;
 };
 
-export default (data1, data2) => iter(data1, data2, []);
+export default (data1, data2) => buildAst(data1, data2, []);
