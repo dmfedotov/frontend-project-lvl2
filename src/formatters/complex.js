39b6333f5d41105ef320c0f873f37f6813484a67
Dmitry <crismsfiva@gmail.com>
2020-02-18 20:16:40 +0300

    fix duplication in module

--- src/formatters/complex.js
+++ src/formatters/complex.js
@@ -1,30 +1,44 @@
-import valueStringify from '../util';
+import { isObject } from 'lodash';
 
-const paddingLeft = '    ';
+const padding = '    ';
+
+const stringify = (value, depth) => {
+  if (isObject(value)) {
+    const keys = Object.keys(value);
+    const newValue = keys.map((key) => `${padding.repeat(depth + 1)}${key}: ${value[key]}\n`).join('');
+    return `{\n${newValue}${padding.repeat(depth)}}\n`;
+  }
+
+  return `${value}\n`;
+};
+
+const renderNode = (node, depth) => {
+  const { type } = node;
+
+  const typeToString = {
+    nested: `${padding.repeat(depth)}${node.name}: {\n`,
+    unchanged: `${padding.repeat(depth)}${node.name}: ${stringify(node.value, depth)}`,
+    deleted: `${padding.repeat(depth - 1)}  - ${node.name}: ${stringify(node.valueBefore, depth)}`,
+    added: `${padding.repeat(depth - 1)}  + ${node.name}: ${stringify(node.valueAfter, depth)}`,
+  };
+
+  if (type === 'changed') {
+    return `${typeToString.deleted}${typeToString.added}`;
+  }
+
+  return typeToString[type];
+};
 
 const renderTree = (diff) => {
   const iter = (node, acc, depth) => node.reduce((inAcc, inNode) => {
-    if (inNode.type === 'unchanged') {
-      if (inNode.children) {
-        return [
-          ...inAcc,
-          [`${paddingLeft.repeat(depth)}${inNode.name}: {\n${iter(inNode.children, [], depth + 1)}${paddingLeft.repeat(depth)}}\n`],
-        ].join('');
-      }
-      return [...inAcc, [`${paddingLeft.repeat(depth)}${inNode.name}: ${inNode.value}\n`]].join('');
-    }
-
-    if (inNode.type === 'added') {
-      return [...inAcc, [`${paddingLeft.repeat(depth - 1)}  + ${inNode.name}: ${valueStringify(inNode.valueAfter, depth)}`]].join('');
-    }
-    if (inNode.type === 'changed') {
+    if (inNode.type === 'nested') {
       return [
         ...inAcc,
-        [`${paddingLeft.repeat(depth - 1)}  - ${inNode.name}: ${valueStringify(inNode.valueBefore, depth)}`],
-        [`${paddingLeft.repeat(depth - 1)}  + ${inNode.name}: ${valueStringify(inNode.valueAfter, depth)}`],
+        `${renderNode(inNode, depth)}${iter(inNode.children, [], depth + 1)}${padding.repeat(depth)}}\n`,
       ].join('');
     }
-    return [...inAcc, [`${paddingLeft.repeat(depth - 1)}  - ${inNode.name}: ${valueStringify(inNode.valueBefore, depth)}`]].join('');
+
+    return [...inAcc, renderNode(inNode, depth)].join('');
   }, acc);
 
   const result = iter(diff, [], 1).trimEnd();
