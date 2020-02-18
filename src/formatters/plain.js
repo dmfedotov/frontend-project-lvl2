4ebcfeacc652da32d7c4059df06c98bdbe3a7bbf
Dmitry <crismsfiva@gmail.com>
2020-02-18 20:54:25 +0300

    refactor plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,37 +1,35 @@
-const getValue = (value) => (value instanceof Object ? '[complex value]' : `'${value}'`);
+import { isObject } from 'lodash';
 
-const buildNode = (name, type, ...values) => {
-  if (type === 'added') {
-    const [value] = values;
-    return `Property '${name}' was added with value: ${getValue(value)}`;
-  }
-  if (type === 'changed') {
-    const [valueBefore, valueAfter] = values;
-    return `Property '${name}' was changed from ${getValue(valueBefore)} to ${getValue(valueAfter)}`;
-  }
+const stringify = (value) => (isObject(value) ? '[complex value]' : `'${value}'`);
 
-  return `Property '${name}' was deleted`;
+const renderNode = (node, name) => {
+  const { type } = node;
+
+  const typeToValue = {
+    added: `Property '${name}' was added with value: ${stringify(node.valueAfter)}`,
+    deleted: `Property '${name}' was deleted`,
+    changed: `Property '${name}' was changed from ${stringify(node.valueBefore)} to ${stringify(node.valueAfter)}`,
+  };
+
+  return typeToValue[type];
 };
 
-const render = (ast) => {
+const renderDiff = (ast) => {
   const iter = (node, acc, ancestry) => node.reduce((inAcc, inNode) => {
-    const { name, type } = inNode;
+    const { name } = inNode;
     const newAncestry = `${ancestry}.${name}`;
 
     if (inNode.children) {
-      return [...inAcc, [iter(inNode.children, [], `${newAncestry}`)]].join('');
+      return [...inAcc, iter(inNode.children, [], `${newAncestry}`)].join('');
     }
     if (inNode.type === 'unchanged') {
       return inAcc;
     }
-    if (type === 'added' || type === 'deleted') {
-      return [...inAcc, [`${buildNode(newAncestry.slice(1), type, inNode.valueAfter)}\n`]].join('');
-    }
 
-    return [...inAcc, [`${buildNode(newAncestry.slice(1), type, inNode.valueBefore, inNode.valueAfter)}\n`]].join('');
+    return [...inAcc, `${renderNode(inNode, newAncestry.slice(1))}\n`].join('');
   }, acc);
 
   return iter(ast, [], '').trimEnd();
 };
 
-export default render;
+export default renderDiff;
