16d4d756b8799f467158a6036a8cfc527180fb85
Dmitry <crismsfiva@gmail.com>
2020-02-21 14:36:26 +0300

    refactor parse function signature

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import parse from './parsers';
 import render from './formatters';
 
 export default (path1, path2, format = 'complex') => {
-  const file1 = fs.readFileSync(path.resolve(process.cwd(), path1), 'utf-8');
-  const file2 = fs.readFileSync(path.resolve(process.cwd(), path2), 'utf-8');
-  const extension = path.extname(path1).slice(1);
+  const data1 = fs.readFileSync(path.resolve(process.cwd(), path1), 'utf-8');
+  const data2 = fs.readFileSync(path.resolve(process.cwd(), path2), 'utf-8');
+  const type = path.extname(path1).slice(1);
 
-  const data1 = parse(extension, file1);
-  const data2 = parse(extension, file2);
-  const diff = buildDiff(data1, data2);
+  const parsedData1 = parse(type, data1);
+  const parsedData2 = parse(type, data2);
+  const diff = buildDiff(parsedData1, parsedData2);
 
-  return render[format](diff);
+  return render(format, diff);
 };
--- src/parsers.js
+++ src/parsers.js
@@ -1,12 +1,10 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-export default (extension, file) => {
-  const extensionToParser = {
-    json: (data) => JSON.parse(data),
-    yml: (data) => yaml.safeLoad(data),
-    ini: (data) => ini.parse(data),
-  };
-
-  return extensionToParser[extension](file);
+const parsers = {
+  json: JSON.parse,
+  yml: yaml.safeLoad,
+  ini: ini.parse,
 };
+
+export default (type, data) => parsers[type](data);
