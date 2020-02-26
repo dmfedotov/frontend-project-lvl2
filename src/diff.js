e82b3e0903dc2576b3d270f7157c35d06d282c05
Dmitry <crismsfiva@gmail.com>
2020-02-26 21:23:22 +0300

    refactor ast building

--- src/diff.js
+++ src/diff.js
@@ -1,32 +1,43 @@
-import { union, isEqual, isObject } from 'lodash';
+import {
+  union, isEqual, isObject, has,
+} from 'lodash';
 
-const buildAst = (data1, data2, acc) => {
+const nodes = [
+  {
+    check: (data1, data2, key) => (isObject(data1[key]) && isObject(data2[key])),
+    buildNode: (name, valueBefore, valueAfter, func) => (
+      { name, type: 'nested', children: func(valueBefore, valueAfter) }
+    ),
+  },
+  {
+    check: (data1, data2, key) => isEqual(data1[key], data2[key]),
+    buildNode: (name, valueBefore, _valueAfter) => ({ name, type: 'unchanged', value: valueBefore }),
+  },
+  {
+    check: (data1, data2, key) => (!has(data1, key) && has(data2, key)),
+    buildNode: (name, _valueBefore, valueAfter) => ({ name, type: 'added', valueAfter }),
+  },
+  {
+    check: (data1, data2, key) => (has(data1, key) && !has(data2, key)),
+    buildNode: (name, valueBefore, _valueAfter) => ({ name, type: 'deleted', valueBefore }),
+  },
+  {
+    check: (data1, data2, key) => !isEqual(data1[key], data2[key]),
+    buildNode: (name, valueBefore, valueAfter) => ({
+      name, type: 'changed', valueBefore, valueAfter,
+    }),
+  },
+];
+
+const buildAst = (data1, data2) => {
   const keys = union(Object.keys(data1), Object.keys(data2)).sort();
 
   const ast = keys.map((key) => {
-    const valueBefore = data1[key];
-    const valueAfter = data2[key];
-    const hasChildren = isObject(valueBefore) && isObject(valueAfter);
-
-    if (hasChildren) {
-      return { name: key, type: 'nested', children: buildAst(valueBefore, valueAfter, []) };
-    }
-    if (isEqual(valueBefore, valueAfter)) {
-      return { name: key, type: 'unchanged', value: valueBefore };
-    }
-    if (!valueBefore) {
-      return { name: key, type: 'added', valueAfter };
-    }
-    if (!valueAfter) {
-      return { name: key, type: 'deleted', valueBefore };
-    }
-
-    return {
-      name: key, type: 'changed', valueBefore, valueAfter,
-    };
-  }, acc);
+    const { buildNode } = nodes.find(({ check }) => check(data1, data2, key));
+    return buildNode(key, data1[key], data2[key], buildAst);
+  });
 
   return ast;
 };
 
-export default (data1, data2) => buildAst(data1, data2, []);
+export default (data1, data2) => buildAst(data1, data2);
