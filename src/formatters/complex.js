c008022842703640f203926411cbc54602265246
Dmitry <crismsfiva@gmail.com>
2020-02-27 12:06:03 +0300

    refactor complex render

--- src/formatters/complex.js
+++ src/formatters/complex.js
@@ -4,45 +4,29 @@ const padding = '    ';
 
 const stringify = (value, depth) => {
   if (!isObject(value)) {
-    return `${value}\n`;
+    return value;
   }
 
   const keys = Object.keys(value);
-  const newValue = keys.map((key) => `${padding.repeat(depth + 1)}${key}: ${value[key]}\n`).join('');
-  return `{\n${newValue}${padding.repeat(depth)}}\n`;
+  const newValue = keys.map((key) => `${padding.repeat(depth + 1)}${key}: ${value[key]}`).join('\n');
+  return `{\n${newValue}\n${padding.repeat(depth)}}`;
 };
 
-const renderNode = (node, depth) => {
-  const { type } = node;
-
-  const typeToValue = {
-    nested: `${padding.repeat(depth)}${node.name}: {\n`,
-    unchanged: `${padding.repeat(depth)}${node.name}: ${stringify(node.value, depth)}`,
-    deleted: `${padding.repeat(depth - 1)}  - ${node.name}: ${stringify(node.valueBefore, depth)}`,
-    added: `${padding.repeat(depth - 1)}  + ${node.name}: ${stringify(node.valueAfter, depth)}`,
-  };
-
-  if (type === 'changed') {
-    return `${typeToValue.deleted}${typeToValue.added}`;
-  }
-
-  return typeToValue[type];
+const typeToString = {
+  nested: ({ name, children }, depth, render) => (
+    `${padding.repeat(depth)}${name}: {\n${render(children, depth + 1)}\n${padding.repeat(depth)}}`
+  ),
+  unchanged: ({ name, value }, depth) => `${padding.repeat(depth)}${name}: ${stringify(value, depth)}`,
+  deleted: ({ name, valueBefore }, depth) => `${padding.repeat(depth - 1)}  - ${name}: ${stringify(valueBefore, depth)}`,
+  added: ({ name, valueAfter }, depth) => `${padding.repeat(depth - 1)}  + ${name}: ${stringify(valueAfter, depth)}`,
+  changed: ({ name, valueBefore, valueAfter }, depth) => (
+    `${padding.repeat(depth - 1)}  - ${name}: ${stringify(valueBefore, depth)}\n${padding.repeat(depth - 1)}  + ${name}: ${stringify(valueAfter, depth)}`
+  ),
 };
 
-const renderDiff = (ast) => {
-  const iter = (node, acc, depth) => node.reduce((inAcc, inNode) => {
-    if (inNode.type === 'nested') {
-      return [
-        ...inAcc,
-        `${renderNode(inNode, depth)}${iter(inNode.children, [], depth + 1)}${padding.repeat(depth)}}\n`,
-      ].join('');
-    }
+const renderDiff = (ast, depth) => ast.map((node) => {
+  const renderNode = typeToString[node.type];
+  return renderNode(node, depth, renderDiff);
+}).join('\n');
 
-    return [...inAcc, renderNode(inNode, depth)].join('');
-  }, acc);
-
-  const result = iter(ast, [], 1).trimEnd();
-  return `{\n${result}\n}`;
-};
-
-export default renderDiff;
+export default (diff) => `{\n${renderDiff(diff, 1)}\n}`;
